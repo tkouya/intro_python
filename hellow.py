052f79eea52d77831baaed25b58594c1e251ee67
Tomonori Kouya <tkouya@users.noreply.github.com>
2024-04-03 20:21:11 +0900

    Delete hellow.py

--- hellow.py
+++
@@ -1,7 +0,0 @@
-# hellow.py: 最初のPythonスクリプト
-print('Hellow, Python!')
-
-# -------------------------------------
-# Copyright (c) 2024 Tomonori Kouya
-# https://github.com/tkouya/intro_python
-# -------------------------------------
