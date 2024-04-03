f5065010693924062244e2930854abfffa11530b
Tomonori Kouya <tkouya@users.noreply.github.com>
2024-04-03 20:18:10 +0900

    Add files via upload

---
+++ print_name.py
@@ -0,0 +1,7 @@
+# print_name.py: 自分の名前をローマ字表記する
+print('Tomonori Kouya')
+
+# -------------------------------------
+# Copyright (c) 2024 Tomonori Kouya
+# https://github.com/tkouya/intro_python
+# -------------------------------------
---
+++ simple_calc3_np.py
@@ -0,0 +1,11 @@
+# simple_calc3_np.py: 加減乗除結果を表示(関数化と名前空間使用)
+import mytool as myt # mytoolモジュールからcalc関数を使用可能にする
+
+# メイン関数部分
+myt.calc(10, 3) # a = 10, b = 3
+myt.calc(3, 4)  # a = 3, b = 4
+
+# -------------------------------------
+# Copyright (c) 2024 Tomonori Kouya
+# https://github.com/tkouya/intro_python
+# -------------------------------------
