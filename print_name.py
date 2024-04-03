a3c7bda12f6fc0c4d5fe6a0e0623a4b2acfabc62
Tomonori Kouya <tkouya@users.noreply.github.com>
2024-04-03 20:23:25 +0900

    Delete print_name.py

--- print_name.py
+++
@@ -1,7 +0,0 @@
-# print_name.py: 自分の名前をローマ字表記する
-print('Tomonori Kouya')
-
-# -------------------------------------
-# Copyright (c) 2024 Tomonori Kouya
-# https://github.com/tkouya/intro_python
-# -------------------------------------
